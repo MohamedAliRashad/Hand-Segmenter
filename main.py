3219117a1be89d8b0268e2545e86095a50b355a0
MohamedAliRashad <m.rashadnow@gmail.com>
2020-04-11 19:06:45 +0200

    Add main for interfacing in the future

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    #TODO: Add Arguments needed
+    
+    args = parser.parse_args()
+
+    main(args)
