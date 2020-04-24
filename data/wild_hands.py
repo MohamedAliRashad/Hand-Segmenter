9c8966ed0a56bc38c714683af09c34cd3afe8457
Ramez <rameznoshy@gmail.com>
2020-04-24 18:17:54 +0200

    remove transform assumption, unnecessary imports

--- data/wild_hands.py
+++ data/wild_hands.py
@@ -2,9 +2,6 @@ import os
 
 from PIL import Image
 from torch.utils.data import Dataset
-from torch.utils.data.dataloader import DataLoader
-from torchvision import transforms
-import matplotlib.pyplot as plt
 
 
 class WildHandsDataset(Dataset):
@@ -17,19 +14,14 @@ class WildHandsDataset(Dataset):
             target and transforms it.
     """
 
-    def __init__(self, dataset_path, level=0, transform=None, target_transform=None):
-        if level:
-            self.dataset_path = os.path.join(dataset_path, f"level_{level}")
-        else:
-            self.dataset_path = dataset_path
-        default_transform = transforms.ToTensor()
-        self.transform = transform if transform is not None else default_transform
-        self.target_transform = target_transform if target_transform is not None else default_transform
+    def __init__(self, dataset_path, level=1, transform=None, target_transform=None):
+        self.dataset_path = os.path.join(dataset_path, f"level_{level}")
+        self.transform = transform
+        self.target_transform = target_transform
         self.img_dirs, self.label_dirs = self.get_img_dirs()
-        self.length = len(self.img_dirs)
 
     def __len__(self):
-        return self.length
+        return len(self.img_dirs)
 
     def __getitem__(self, idx):
         """
@@ -40,10 +32,10 @@ class WildHandsDataset(Dataset):
             tuple: (image, target) where target is the same image.
         """
         img = Image.open(self.img_dirs[idx]).convert("RGB")
-        img = self.transform(img)
+        img = self.transform(img) if self.transform else img
 
         target = Image.open(self.label_dirs[idx]).convert("RGB")  # not sure mn el convert tbh
-        target = self.transform(target)
+        target = self.target_transform(target) if self.target_transform else target
         return img, target
 
     def get_img_dirs(self):
@@ -51,7 +43,7 @@ class WildHandsDataset(Dataset):
         labels = []
         for dir_path, sub_dirs, files in os.walk(self.dataset_path):
             for file in files:
-                if 'ask' in file:
+                if 'ask' in file and (os.path.splitext(file)[1]).lower() == '.png':
                     labels.append(os.path.join(dir_path, file))
                 else:
                     imgs.append(os.path.join(dir_path, file))
@@ -62,6 +54,8 @@ class WildHandsDataset(Dataset):
 DATASET_PATH = r"..\..\Dataset\In the Wild - RGB Segmentation"
 
 if __name__ == "__main__":
+    from torch.utils.data.dataloader import DataLoader
+
     wild_dataset = WildHandsDataset(DATASET_PATH, level=1)
     print(len(wild_dataset))
     data_loader = DataLoader(dataset=wild_dataset, batch_size=32, shuffle=True, num_workers=4, drop_last=True)
