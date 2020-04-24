35b53e18331fe8ed8bb221f0bc8031d4288b7bf4
Ramez <rameznoshy@gmail.com>
2020-04-24 16:22:08 +0200

    implement Dataset class

--- data/wild_hands.py
+++ data/wild_hands.py
@@ -2,6 +2,9 @@ import os
 
 from PIL import Image
 from torch.utils.data import Dataset
+from torch.utils.data.dataloader import DataLoader
+from torchvision import transforms
+import matplotlib.pyplot as plt
 
 
 class WildHandsDataset(Dataset):
@@ -13,11 +16,21 @@ class WildHandsDataset(Dataset):
         target_transform (callable, optional): A function/transform that takes in the
             target and transforms it.
     """
-    def __init__(self, dataset_path, transform=None, target_transform=None):
-        pass
+
+    def __init__(self, dataset_path, level=0, transform=None, target_transform=None):
+        if level:
+            self.dataset_path = os.path.join(dataset_path, f"level_{level}")
+        else:
+            self.dataset_path = dataset_path
+        default_transform = transforms.ToTensor()
+        self.transform = transform if transform is not None else default_transform
+        self.target_transform = target_transform if target_transform is not None else default_transform
+        self.img_dirs, self.label_dirs = self.get_img_dirs()
+        self.length = len(self.img_dirs)
 
     def __len__(self):
-        pass
+        return self.length
+
     def __getitem__(self, idx):
         """
         Args:
@@ -26,8 +39,35 @@ class WildHandsDataset(Dataset):
         Returns:
             tuple: (image, target) where target is the same image.
         """
-        pass
+        img = Image.open(self.img_dirs[idx]).convert("RGB")
+        img = self.transform(img)
+
+        target = Image.open(self.label_dirs[idx]).convert("RGB")  # not sure mn el convert tbh
+        target = self.transform(target)
+        return img, target
+
+    def get_img_dirs(self):
+        imgs = []
+        labels = []
+        for dir_path, sub_dirs, files in os.walk(self.dataset_path):
+            for file in files:
+                if 'ask' in file:
+                    labels.append(os.path.join(dir_path, file))
+                else:
+                    imgs.append(os.path.join(dir_path, file))
+        return imgs, labels
+
 
 # Testing
+DATASET_PATH = r"..\..\Dataset\In the Wild - RGB Segmentation"
+
 if __name__ == "__main__":
-    pass
+    wild_dataset = WildHandsDataset(DATASET_PATH, level=1)
+    print(len(wild_dataset))
+    data_loader = DataLoader(dataset=wild_dataset, batch_size=32, shuffle=True, num_workers=4, drop_last=True)
+    data_iter = iter(data_loader)
+    tensor_image, tensor_label = next(data_iter)
+    print(tensor_image.shape)
+    # todo imshow doesnt work
+    # plt.imshow(tensor_image.permute(1, 2, 0))
+    # plt.show()
