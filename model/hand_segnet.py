8ffbb4e0ee105b5e55892d7467b0231b0c4f6294
MohamedAliRashad <m.rashadnow@gmail.com>
2020-06-18 23:53:58 +0200

    Add a model interface

--- model/hand_segnet.py
+++ model/hand_segnet.py
@@ -1,5 +1,3 @@
-import cv2
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -79,58 +77,28 @@ class HandSegNet(nn.Module):
         return out
 
 
-def maskIt(model, img, use_gpu=True):
+def handsegnet(pretrained=False, progress=True, **kwargs):
+    """
+    Load `HandSegNet` model architecture 
 
-    # device to run the model on
-    device = "cuda" if torch.cuda.is_available() and use_gpu else "cpu"
-    model.to(device)
-
-    # convort BGR fromat to RGB
-    image_raw = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    # compress input image for faster computation and less memory
-    image_v = cv2.resize(image_raw, (320, 240))
-
-    # normalize
-    image_v = np.expand_dims((image_v.astype("float") / 255.0) - 0.5, 0)
-
-    # rearrange dimensions to work with pytorch
-    image_v = torch.from_numpy(image_v.transpose(0, 3, 1, 2)).float()
-
-    # inference
-    hand_scoremap_v = model(image_v.to(device))
-
-    # rearrange output dimensions to be image like
-    hand_scoremap_v = hand_scoremap_v.permute(0, 2, 3, 1).detach().cpu().numpy()
-
-    # remove the extra dimension (batches dim)
-    hand_scoremap_v = np.squeeze(hand_scoremap_v)
-
-    # get the highest class prob for every pixel
-    hand_scoremap_v = np.argmax(hand_scoremap_v, 2)
-
-    return hand_scoremap_v
+    Args:
+    ---
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    model = HandSegNet(**kwargs)
+    weights_url = "https://drive.google.com/uc?export=download&id=1P_ZYUWuUjt2DAUtPxG882mdQX95TIX3D"
+    if pretrained:
+        state_dict = torch.hub.load_state_dict_from_url(weights_url, progress=progress)
+        model.load_state_dict(state_dict)
+    return model
 
 
 # Testing
 if __name__ == "__main__":
-    # Initialize model with trained weights
-    weights_url = "https://drive.google.com/uc?export=download&id=1P_ZYUWuUjt2DAUtPxG882mdQX95TIX3D"
-    seg = HandSegNet()
-    seg.load_state_dict(torch.hub.load_state_dict_from_url(weights_url))
-    seg.eval()
-    
-    # # Dummy Test ...
-    # input_size = (1, 3, 255, 255)
-    # dummy = torch.randn(input_size)
-    # print(seg(dummy).shape)
 
-    # Live Demo ...
-    cap = cv2.VideoCapture(0)
-    key = " "
-    while key != 113:
-        ret, frame = cap.read()
-        mask = maskIt(seg, frame)
-        cv2.imshow("mask", mask.astype(np.float))
-        cv2.imshow("frame", frame)
-        key = cv2.waitKey(1)
+    # dummy test to make sure everything works
+    seg = handsegnet()
+    input_size = (1, 3, 255, 255)
+    dummy = torch.randn(input_size)
+    print(seg(dummy).shape)
