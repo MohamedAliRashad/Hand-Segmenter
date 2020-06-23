5614e2d8e0aa8022287ddc0c4ed247bf4c7f620e
Ramez <rameznoshy@gmail.com>
2020-06-23 20:48:22 +0200

    feat: evaluate a list of data_loaders

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -29,36 +29,36 @@ class Evaluator:
         self.table = Texttable()
         self.table.add_rows([["Dataset", "IoU (Jaccard)", "F1 score (Dice)"]])
 
-    def __call__(self, data_loader):
-        data_iter = iter(data_loader)
-        total_iou = 0
-        total_f_score = 0
-        iterable = tqdm(enumerate(data_iter)) if self.is_progress_bar else enumerate(data_iter)
-        for index, (img, ground_truth) in iterable:
-            prediction = self.model(img)
-            self.current_intersection = torch.sum(prediction * ground_truth)
-            self.total_pixels = torch.sum(ground_truth) + torch.sum(prediction)
-            self.current_union = self.total_pixels - self.current_intersection
-            total_iou += self.__iou()
-            total_f_score += self.__f_score()
-        self.mean_iou = total_iou / len(data_loader)
-        self.mean_f_score = total_f_score / len(data_loader)
-        self.dataset_name = type(data_loader.dataset).__name__
-        self.add_row_to_table()
+    def __call__(self, data_loaders):
+        if type(data_loaders) is not list:
+            data_loaders = [data_loaders]
+        for loader in data_loaders:
+            data_iter = iter(loader)
+            total_iou = 0
+            total_f_score = 0
+            iterable = tqdm(data_iter) if self.is_progress_bar else data_iter
+            for (img, ground_truth) in iterable:
+                prediction = self.model(img)
+                self.current_intersection = torch.sum(prediction * ground_truth)
+                self.total_pixels = torch.sum(ground_truth) + torch.sum(prediction)
+                self.current_union = self.total_pixels - self.current_intersection
+                total_iou += self.__iou()
+                total_f_score += self.__f_score()
+            self.mean_iou = total_iou / len(loader)
+            self.mean_f_score = total_f_score / len(loader)
+            self.dataset_name = type(loader.dataset).__name__.strip('Dataset')
+            self.add_row_to_table()
+        print(self.table.draw())
 
     def __iou(self):
         return self.current_intersection / \
-               ((self.current_union - self.current_intersection) + self.epsilon)
+               (self.current_union + self.epsilon)
 
     def __f_score(self):
         return 2 * self.current_intersection / (self.total_pixels + self.epsilon)
 
     def add_row_to_table(self):
-        self.table.add_rows([["", self.mean_iou, self.mean_f_score]])
-        print(self.table.draw())
-
-    def print_metrics(self):
-        print(self.table.draw())
+        self.table.add_row([self.dataset_name, self.mean_iou, self.mean_f_score])
 
     @staticmethod
     def iou(prediction, ground_truth, epsilon=1e-9):
